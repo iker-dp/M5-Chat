2a2dd515e8eb3a03808ec489141fb0f5f6297609
iker-dp <iker.diaz@insbaixcamp.cat>
2024-04-18 21:26:22 +0200

    Using SQL

--- server.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-
-var app = express();
-
-var servidor = app.listen(3000, () => {
-    console.log('Server running on port ', servidor.address().port);
-   });
-
-app.use(express.static(__dirname));
